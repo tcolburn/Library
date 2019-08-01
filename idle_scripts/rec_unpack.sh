8be7989466847f3a7b4a317706a2531143b898d2
tcolburn <tcolburn@asu.edu>
2019-07-31 18:55:14 -0700

    Delete rec_unpack.sh

--- idle_scripts/rec_unpack.sh
+++
@@ -1,3 +0,0 @@
-find ./wedge/ -name "rec*.bz2" -type f -exec bunzip2 -v {} \;
-find ./spuddafett/ -name "rec*.bz2" -type f -exec bunzip2 -v {} \;
-find ./palpoutine/ -name "rec*.bz2" -type f -exec bunzip2 -v {} \;
