cf8cc0fe79de8eba33e8443e25b7a809f0ca45f3
tcolburn <tcolburn@asu.edu>
2019-07-31 18:55:04 -0700

    Delete dcd_transfer.sh

--- idle_scripts/dcd_transfer.sh
+++
@@ -1,7 +0,0 @@
-for i in {1..5}
-do
-  DIRS=$(sed -n "${i}p" dir_list.txt)
-  cd $DIRS
-  cp full_dims_mhp1_occ2if.dcd /nfs/homes/tcolburn/Projects/Beckstein/Mhp1/simulations/dims/implicit_solvent/implicit_membrane/bench/1_core_psa/
-  cd /nfs/homes/tcolburn/Projects/Beckstein/Mhp1/simulations/dims/implicit_solvent/implicit_membrane/bench/test_1
-done
