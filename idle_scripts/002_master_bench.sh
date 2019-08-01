784b575c8dd5d23ce3f01b886dbf49111422075c
tcolburn <tcolburn@asu.edu>
2019-07-31 18:54:35 -0700

    Delete 002_master_bench.sh

--- idle_scripts/002_master_bench.sh
+++
@@ -1,4 +0,0 @@
-find ./mashteryoda/002/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@mashteryoda {} \;
-find ./palpoutine/002/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@palpoutine {} \;
-find ./spuddafett/002/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@spuddafett {} \;
-find ./wedge/002/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@wedge {} \;
