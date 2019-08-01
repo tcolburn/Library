80e9efe77b97aac766c4022c4abac34cba1ed993
tcolburn <tcolburn@asu.edu>
2019-07-31 18:55:25 -0700

    Delete unpack.sh

--- idle_scripts/unpack.sh
+++
@@ -1,3 +0,0 @@
-find ./wedge/001/ -name "*.bz2" -type f -exec bunzip2 -v {} \;
-find ./spuddafett/001/ -name "*.bz2" -type f -exec bunzip2 -v {} \;
-find ./palpoutine/001/ -name "*.bz2" -type f -exec bunzip2 -v {} \;
