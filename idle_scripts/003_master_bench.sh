2c7e22f6ff0c25492187b0cc90da6c4b4b88e949
tcolburn <tcolburn@asu.edu>
2019-07-31 18:54:45 -0700

    Delete 003_master_bench.sh

--- idle_scripts/003_master_bench.sh
+++
@@ -1,4 +0,0 @@
-find ./frywalker/003/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@frywalker {} \;
-find ./chipbacca/003/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@chipbacca {} \;
-find ./spudda/003/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@spudda {} \;
-find ./yamsolo/003/ -name "dims_seed_bench.ge" -type f -exec qsub_dependents.py -N 6 -- -q workstations.q@yamsolo {} \;
